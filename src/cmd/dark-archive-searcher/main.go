38c418f148a6306ce76b1d1851a49277c069d11d
Jeremy Echols <jechols@uoregon.edu>
2017-11-20 09:21:20 -0800

    Update usage for planned customizations

--- src/cmd/dark-archive-searcher/main.go
+++ src/cmd/dark-archive-searcher/main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/uoregon-libraries/gopkg/fileutil"
+	"github.com/uoregon-libraries/gopkg/wordutils"
 )
 
+var spaces = regexp.MustCompile(`\s+`)
+
+func perrraw(s string) {
+	fmt.Fprintln(os.Stderr, s)
+}
+
 func perr(s string) {
-	fmt.Fprintf(os.Stderr, s+"\n")
+	s = strings.TrimSpace(s)
+	s = spaces.ReplaceAllString(s, " ")
+	perrraw(wordutils.Wrap(s, 80))
 }
 func perrf(s string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, s+"\n", args)
+	perr(fmt.Sprintf(s, args...))
 }
 
 func usage(msg string) {
@@ -22,7 +33,32 @@ func usage(msg string) {
 		status = 1
 	}
 
-	perrf("Usage: %s <path to dark archive>", os.Args[0])
+	perrf("Usage: %s <DA root> <format> <inv pattern>", os.Args[0])
+	perr("")
+	perr(`
+		"DA root" should be the path to the root of the dark archive.  This will be
+		stripped from all indexed data in order to avoid problems if the mount
+		point to the dark archive changes.
+	`)
+	perr("")
+	perr(`
+		"format" should express the path using the keywords "project" and "ignore".
+		There must be exactly one occurrence of "project", designating which path
+		element specifies the project name.  There can be any number of "ignore"
+		elements in the path, each of which are simply ignored in order to form the
+		"public" path.  e.g., "project/ignore/ignore" would state that the
+		top-level folder is the project name and the next two folders are
+		irrelevant.  "ignore/project/ignore" might be used for
+		"Volume/project/date" style archives.
+	`)
+	perr("")
+	perr(`
+		"inv pattern" should simply be a pattern to find all the inventory files,
+		such as "*/*/INVENTORY/*.csv".
+	`)
+	perr("")
+	perr("Example:")
+	perrraw(fmt.Sprintf(`    %s /mnt/darkarchive ignore/project/ignore "*/*/INVENTORY/*.csv"`, os.Args[0]))
 
 	os.Exit(status)
 }
