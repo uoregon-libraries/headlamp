040f9fa1eaf0b5dc47d5d16f1f323d159a960bf7
Jeremy Echols <jechols@uoregon.edu>
2017-12-08 10:16:23 -0800

    Show relative paths in search results

--- src/cmd/headlamp/template_funcs.go
+++ src/cmd/headlamp/template_funcs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"db"
 	"fmt"
+	"strings"
 
 	"github.com/uoregon-libraries/gopkg/tmpl"
 )
@@ -12,6 +13,7 @@ var localTemplateFuncs = tmpl.FuncMap{
 	"BrowseFolderPath":           browseFolderPath,
 	"BrowseContainingFolderPath": browseContainingFolderPath,
 	"DownloadFilePath":           downloadFilePath,
+	"stripProjectFolder":         stripProjectFolder,
 }
 
 // browseProjectPath produces the URL to browse the given project's top-level folder
@@ -29,4 +31,15 @@ func browseContainingFolderPath(file *db.File) string {
 
 func downloadFilePath(file *db.File) string {
 	return fmt.Sprintf("/download/%s/%s", file.Project.Name, file.PublicPath)
+}
+
+// stripProjectFolder takes a string representing a path, and strips out the
+// current folder context, if any exists
+func stripProjectFolder(f *db.Folder, path string) string {
+	if f == nil {
+		return path
+	}
+	path = strings.TrimPrefix(path, f.Path)
+	path = strings.TrimPrefix(path, "/") // Just to make sure there's no starting slash
+	return path
 }
