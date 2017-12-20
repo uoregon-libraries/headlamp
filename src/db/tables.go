84fdf2730d05371bb65f7aeacb57bd7b05a226f5
Jeremy Echols <jechols@uoregon.edu>
2017-12-20 10:37:17 -0800

    Show filesize in a human-friendly way

--- src/db/tables.go
+++ src/db/tables.go
@@ -1,6 +1,9 @@
 package db
 
-import "path/filepath"
+import (
+	"fmt"
+	"path/filepath"
+)
 
 // Project maps to the projects database table, which represents a "magic"
 // dark-archive directory we expose as if it's a top-level directory for
@@ -48,6 +51,21 @@ type File struct {
 	Name        string
 	FullPath    string
 	PublicPath  string
+}
+
+// HumanFilesize returns a more meaningful value for filesizes
+func (f *File) HumanFilesize() string {
+	var suffixes = []string{"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
+	var size = float64(f.Filesize)
+	var idx = 0
+	for size > 1024 && idx < len(suffixes) {
+		idx++
+		size = size / 1024.0
+	}
+	if idx == 0 {
+		return fmt.Sprintf("%d %s", f.Filesize, suffixes[idx])
+	}
+	return fmt.Sprintf("%0.2f %s", size, suffixes[idx])
 }
 
 // ContainingFolder returns the path to the file's folder for cases where
