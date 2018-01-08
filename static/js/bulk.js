9cbd02945d7313a9cfc10bc94a4d7b2e6797b58a
Jeremy Echols <jechols@uoregon.edu>
2018-01-08 12:10:09 -0800

    Make bulk queue work on all browsers
    Array.prototype.forEach isn't working in some browsers where it should
    work (Edge on a tablet).  Iterating over a list while keeping the button
    within the closure required a new function.  This is ugly, but I think
    it fixes the seemingly random bulk queue failures.

--- static/js/bulk.js
+++ static/js/bulk.js
@@ -1,23 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
   var bulkButtons = document.querySelectorAll("button.bulk-action");
-  bulkButtons.forEach(function(btn) {
-    btn.addEventListener("click", function(e) {
-      btn.setAttribute("disabled", "disabled");
-      var postLocation = btn.dataset["action"];
-      fetch(postLocation, {method: "POST", credentials: "same-origin"}).then(function(response) {
-        console.log(response);
-        if (response.status == 200) {
-          var id = btn.dataset["toggleOnSuccess"];
-          console.log("ID is " + id);
-          var el = document.getElementById(id);
-          console.log(el);
-          el.removeAttribute("disabled");
-        }
-        else {
-          btn.removeAttribute("disabled");
-          alert("Error!  Please try again or contact support.");
-        }
-      });
+  for (var i = 0; i < bulkButtons.length; i++) {
+    var btn = bulkButtons[i];
+    btn.addEventListener("click", clickCallback(btn));
+  }
+})
+
+function clickCallback(btn) {
+  return function(e) {
+    btn.setAttribute("disabled", "disabled");
+    var postLocation = btn.dataset["action"];
+    fetch(postLocation, {method: "POST", credentials: "same-origin"}).then(function(response) {
+      console.log(response);
+      if (response.status == 200) {
+        var id = btn.dataset["toggleOnSuccess"];
+        console.log("ID is " + id);
+        var el = document.getElementById(id);
+        console.log(el);
+        el.removeAttribute("disabled");
+      }
+      else {
+        btn.removeAttribute("disabled");
+        alert("Error!  Please try again or contact support.");
+      }
     });
-  });
-});
+  };
+}
