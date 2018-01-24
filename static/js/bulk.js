6adec9804bc54819e498ef471efc51a3b3ec3702
Jeremy Echols <jechols@uoregon.edu>
2018-01-24 08:53:50 -0800

    Do all bulk actioning in the clickCallback

--- static/js/bulk.js
+++ static/js/bulk.js
@@ -5,14 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var btn = bulkButtons[i];
     btn.addEventListener("click", clickCallback(btn));
   }
-
-  // Special behavior for the "remove" buttons on the bulk download page
-  var bulkRows = document.querySelectorAll(".bulk-row");
-  for (var i = 0; i < bulkRows.length; i++) {
-    var row = bulkRows[i];
-    var btn = row.querySelector("button.bulk-action[data-is-remove='1']");
-    btn.addEventListener("click", removeRowCallback(row));
-  }
 })
 
 function clickCallback(btn) {
@@ -28,17 +20,20 @@ function clickCallback(btn) {
 
       var id = btn.dataset["toggleOnSuccess"];
       var el = document.getElementById(id);
+
       // On the bulk downloads page, we don't have a "Queue" button, so we
       // have to check for null elements
       if (el != null) {
         el.removeAttribute("disabled");
       }
-    });
-  };
-}
 
-function removeRowCallback(row) {
-  return function(e) {
-    row.remove();
+      // bulk-downloads needs to hide the row
+      if (btn.dataset["isRemove"] == "1") {
+        var row = btn.closest(".bulk-row");
+        if (row != null) {
+          row.remove();
+        }
+      }
+    });
   };
 }
